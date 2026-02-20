16fa6253ee68c3de1a130aedb88b26e4dd47d921
Holycoder02 <gaurabhkumar0@gmail.com>
2026-02-20 17:03:18 +0530

    done

--- python/control Statement/loop/range.py
+++ python/control Statement/loop/range.py
@@ -10,5 +10,33 @@ step - 1
 
 
 """
-a = tuple(range(1, 100, 1))
-print(a)
+#a = tuple(range(1, 100, 1))
+#print(a)
+
+
+#listcompersion
+
+"""
+[expersion for iteam in itreable if condition]
+
+e - x * 2
+iteam -
+itrable - range(1,11)
+contidion optional   #like-  if , else if else,,,,
+
+"""
+"""
+squares =[]
+for i in range(1,11):
+    squares.append(i ** 2)
+    print(squares)
+
+
+squares = [i ** 2 for i in range(1,11)]
+print(squares)
+"""
+
+#squares of all even number 
+
+squares = [i ** 2 for i in range(1,40) if i%2 == 0]
+print(squares)
