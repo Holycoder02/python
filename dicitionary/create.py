352acb0eee443b135397a1caab3e8a45c13b285b
Holycoder02 <gaurabhkumar0@gmail.com>
2026-02-20 17:15:23 +0530

    dicttxt

---
+++ dicitionary/create.py
@@ -0,0 +1,27 @@
+#[], (), {}
+"""
+my_dict ={
+    "name":"sagar","Age":100, "marks":50.55, 100:"abc"
+}
+print(my_dict)
+"""
+
+
+#in this example we can see the key pair should be unique...
+"""
+my_dict1 = {
+    "marks":[1,2,3,4], "marks":100
+}
+print(my_dict1)
+"""
+
+
+#dicitionary for adding element
+my_dict2 = {
+    'fruites':['kela', 'apple', 'orange'], 'category':'fruit'
+}
+print(my_dict2)
+
+my_dict2['price']= 100
+print(my_dict2)
+
---
+++ dicitionary/update.py
@@ -0,0 +1,19 @@
+"""
+my_dict = {
+    'name':'python', 'version':'3.9'
+}
+my_dict['version'] = 4.1
+print(my_dict)
+"""
+
+"""
+for deleting in dict
+
+"""
+
+my_dict = {
+    'name':'python', 'version':3.9, 'use_Case':['AI','ML', 'DSA', 'WD']
+}
+print(my_dict)
+del my_dict['version']
+print(my_dict)
