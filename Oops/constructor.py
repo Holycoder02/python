cb5ee5fbfb520cdb399e4068e0d76c5be5dbe075
Holycoder02 <gaurabhkumar0@gmail.com>
2026-03-31 11:57:19 +0530

    Checkpoint from VS Code for cloud agent session

--- Oops/constructor.py
+++ Oops/constructor.py
@@ -1,3 +1,17 @@
+#example for  not use constructor and use method to initialize the value
+
 '''
 __int__()
 '''
+
+class Car():
+    def __init__(self, brand, color):
+        self.brand = brand
+        self.color = color
+
+#creating objects
+car1 = Car()
+car1 = Car('tesla', 'red')
+
+print(car1.brand)
+print(car1.color)
